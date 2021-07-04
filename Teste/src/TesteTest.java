6ec14a52b25547a6639179131b67295f324171bf
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-07-04 17:18:30 -0300

    easda

---
+++ Teste/src/TesteTest.java
@@ -0,0 +1,17 @@
+
+public class TesteTest {
+
+	public static void main(String[] args) {
+		String name = "Breno";
+		int a = 1;
+		int b = 2;
+		
+		
+		
+		while(a < 10) {
+			System.out.println(a++);
+		}
+
+	}
+
+}
